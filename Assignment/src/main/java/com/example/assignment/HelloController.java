50b995d5ecd7787b3cd6e72d5b2fd6fff70b3dd7
Adam Greenan <87552818+plutowootoe@users.noreply.github.com>
2022-11-27 17:31:42 +0000

    Add files via upload

---
+++ Assignment/src/main/java/com/example/assignment/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.assignment;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    protected void getButton(){
+        return;
+    }
+    @FXML
+    protected void OnButtonPress() {
+       //
+    }
+}
